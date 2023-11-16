57ad309291e70ec2150650d08fa45d7d8488db7a
camil3911 <150868194+camil3911@users.noreply.github.com>
2023-11-16 23:44:46 +0000

    ajoutez calculatrice

--- Calculatrice.java
+++ Calculatrice.java
@@ -1,3 +1,23 @@
-public class Calculatrice {
-  
+import java.util.Scanner;
+
+public class Calculatrice { 
+    public static void main(String[] args) {
+  Scanner scanner=new Scanner(System.in);
+
+
+  System.out.println("Veuillez entrez votre premier numéro : ");
+  double numéro1=scanner.nextDouble();
+  System.out.println("Veuillez entrez votre deuxième numéro : ");
+  double numéro2=scanner.nextDouble();
+  double somme=numéro1+numéro2;
+  double produit=numéro1*numéro2;
+  double différence=numéro1-numéro2;
+  double quotient=numéro1/numéro2;
+
+
+  System.out.println("La somme des deux chiffres est : "+ somme);
+  System.out.println("Le produit des deux chiffres est : "+ produit);
+  System.out.println("La différence des deux chiffres est : "+ différence);
+  System.out.println("Le quotient des deux chiffres est : "+ quotient);
+}
 }
